5455f48a8c8ceed9aa13ebdc31a17436239c122f
Jonathan Fritz <fritz@nosc.io>
2022-04-10 20:45:22 +0200

    Updating readme

--- src/rendering/render_preparing.c
+++ src/rendering/render_preparing.c
@@ -6,7 +6,7 @@
 /*   By: jfritz <jfritz@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/07 17:08:48 by jfritz            #+#    #+#             */
-/*   Updated: 2022/04/10 20:41:37 by jfritz           ###   ########.fr       */
+/*   Updated: 2022/04/10 20:43:42 by jfritz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,7 +47,7 @@ int	render_loop(void	*cub_)
 void	set_moverot_speeds(t_cub *cub)
 {
 	cub->math->moveSpeed = 1.0;
-	cub->math->rotSpeed = 1.0;
+	cub->math->rotSpeed = 0.1;
 }
 
 /**
