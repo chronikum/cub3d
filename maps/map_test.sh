ceee359dbf4c040ae8274a3182f63d2951270bad
Jonathan Fritz <fritz@nosc.io>
2022-04-13 15:55:33 +0200

    Adding make map_test

--- maps/map_test.sh
+++ maps/map_test.sh
@@ -1,12 +1,31 @@
-echo "Following tests should all fail and exit."
+echo "Please run me like this ./maps/map_test.sh"
+echo "Following maps should all fail and exit."
 
-../cub3D ./maps/double_identifier.cub
-../cub3D ./maps/double_rgb.cub
-../cub3D ./maps/emptymap.cub
-../cub3D ./maps/invalid_symbol.cub
-../cub3D ./maps/invalid_texture.cub
-../cub3D ./maps/multiple_player.cub
-../cub3D ./maps/non_square_tex.cub
-../cub3D ./maps/unknown_key.cub
-../cub3D ./maps/wrong_rgb.cub
-../cub3D ./maps/wrong_rgb2.cub
+function_check_error () {
+	if [ "$?" -ne 0 ]; then
+		echo "TEST OK: $1";
+	else
+		echo "TEST FAIL: $1";
+	fi
+}
+
+./cub3D ./maps/double_identifier.cub > /dev/null 2>&1
+function_check_error "Double ID"
+./cub3D ./maps/double_rgb.cub > /dev/null 2>&1
+function_check_error "Double RGB"
+./cub3D ./maps/emptymap.cub > /dev/null 2>&1
+function_check_error "Empty map"
+./cub3D ./maps/invalid_symbol.cub > /dev/null 2>&1
+function_check_error "invalid symbol in map"
+./cub3D ./maps/invalid_texture.cub > /dev/null 2>&1
+function_check_error "invalid texture"
+./cub3D ./maps/multiple_player.cub > /dev/null 2>&1
+function_check_error "double player"
+./cub3D ./maps/non_square_tex.cub > /dev/null 2>&1
+function_check_error "non square texture"
+./cub3D ./maps/unknown_key.cub > /dev/null 2>&1
+function_check_error "unknown direction key"
+./cub3D ./maps/wrong_rgb.cub > /dev/null 2>&1
+function_check_error "overflow rgb value"
+./cub3D ./maps/wrong_rgb2.cub > /dev/null 2>&1
+function_check_error "underflow rgb value"
