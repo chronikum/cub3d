e9e595ec91ff78446da2ff23b0add443a3472e74
Jonathan Fritz <fritz@nosc.io>
2022-04-13 14:24:10 +0200

    Add map tester

---
+++ maps/map_test.sh
@@ -0,0 +1,12 @@
+echo "Following tests should all fail and exit."
+
+../cub3D ./maps/double_identifier.cub
+../cub3D ./maps/double_rgb.cub
+../cub3D ./maps/emptymap.cub
+../cub3D ./maps/invalid_symbol.cub
+../cub3D ./maps/invalid_texture.cub
+../cub3D ./maps/multiple_player.cub
+../cub3D ./maps/non_square_tex.cub
+../cub3D ./maps/unknown_key.cub
+../cub3D ./maps/wrong_rgb.cub
+../cub3D ./maps/wrong_rgb2.cub
